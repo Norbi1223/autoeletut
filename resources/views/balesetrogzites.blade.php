bea4729ded33a771790614c2a417aa1ae50436b9
Norbert1223 <knorbert1223@gmail.com>
2022-11-16 17:46:26 +0100

    formok befejezese

--- resources/views/balesetrogzites.blade.php
+++ resources/views/balesetrogzites.blade.php
@@ -9,16 +9,12 @@
             <form method="POST">
                 @csrf
                 <div class="m-2 p-1">
-                <label for="rendszam"><b>Rendszám:</b></label>
-                <input type="text" name="rendszam" id="rendszam" class="form-control">
+                    <label for="balido"><b>Baleset időpontja:</b></label>
+                    <input type="datetime" name="balido" id="balido" class="form-control">
                 </div>
                 <div class="m-2 p-1">
-                    <label for="rendszam"><b>Típus:</b></label>
-                    <input type="text" name="tipus" id="tipus" class="form-control">
-                </div>
-                <div class="m-2 p-1">
-                    <label for="rendszam"><b>Szín:</b></label>
-                    <input type="text" name="szin" id="szin" class="form-control">
+                    <label for="serules"><b>Sérülés leírása:</b></label>
+                    <input type="text" name="serules" id="serules" class="form-control">
                 </div>
                 <div class="m-2 p-1">
                     <button type="submit" class="btn btn-success">Rögzítés</button>
